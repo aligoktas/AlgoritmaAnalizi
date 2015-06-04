1041affa0377a8c1b10b50000ceaeccf58efde26
Ali Göktaş <aligoktass16@gmail.com>
2015-06-04 19:13:05 +0300

    Final Sınav Sorular

---
+++ FinalSoru6/FinalSoru6/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalSoru6
+{
+    class Program
+    {
+        public static int[] altdiziBul(int [] array)
+        {
+            int sayac = 0;
+            int ilkindis = 0;
+            int sonindis = 0;
+            int kontrol = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]==0 && array[i+1]==0)
+                {
+                    sayac++;
+                }
+                if (sayac >= kontrol && array[i]==1)
+                {
+                    kontrol = sayac+1;
+                 
+                    sonindis = i-1;
+                    ilkindis = sonindis - sayac;
+                    sayac = 0;
+                   
+                }
+            }
+            int [] dizi= new int[kontrol]; 
+            int j = 0;
+            for (int i = ilkindis; i <= sonindis; i++)
+            {
+                dizi[j] = array[i];
+                j++;
+
+            }
+            return dizi;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 0,0,0 ,1, 0, 1, 0, 1,0,0, 1 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+
+            }
+
+            Console.WriteLine(" ");
+
+            int[] altdizi = altdiziBul(array);
+            for (int i = 0; i < altdizi.Length; i++)
+            {
+                Console.Write(altdizi[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FinalSoru8/FinalSoru8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalSoru8
+{
+    class Program
+    {
+        public static void resize (ref long[] array, int n)
+        { 
+            int newsize = array.Length * n;
+            Array.Resize(ref array, newsize);
+            
+      }
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+            long[] dizi = new long[1];
+
+            Console.WriteLine("Dizi Kaç elemanlı olsun.");
+            a = Convert.ToInt32(Console.ReadLine());
+          
+            resize(ref dizi, a);
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < a; i++)
+            {
+                dizi[i] = rnd.Next(100);
+            
+            }
+            
+            for (int i = a-1; i >= 0; i--)
+            {
+                Console.WriteLine(i + " . eleman =" + dizi[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
